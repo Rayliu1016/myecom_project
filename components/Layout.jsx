9e7634ecaa6eb493b0d117942826fa34ef741139
Raymond <rayliu1016@gmail.com>
2022-07-05 10:47:29 -0400

    fixed minor error

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -8,7 +8,7 @@ const Layout = ({children}) => {
   return (
     <div className='layout'>
         <Head>
-            <title>Raymond's Headphones</title>
+            <title>Raymond Headphones</title>
         </Head>
         <header>
             <Navbar/>
